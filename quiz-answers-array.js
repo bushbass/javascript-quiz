3d72e08e60b66ace234255172133c183d4164b7e
bushbass <alex.nielsen@gmail.com>
2019-01-02 14:01:29 -0500

    working on multiple choice version

---
+++ quiz-answers-array.js
@@ -0,0 +1,32 @@
+let questionArray = [
+  {
+    question: 'How many fish do I have?',
+    a: 'One',
+    b: 'Two',
+    c: 'Three',
+    correctAnswer: 'Two'
+  },
+  {
+    question: "What is my dog's name",
+    a: 'Lassie',
+    b: 'Red',
+    c: 'Fido',
+    correctAnswer: 'Red'
+  },
+  {
+    question: "What is my cat's name",
+    a: 'Lily',
+    b: 'Milo',
+    c: 'Otis',
+    correctAnswer: 'Lily'
+  },
+  {
+    question: 'How many planets are there in our solar system',
+    a: '7',
+    b: '8',
+    c: '9',
+    correctAnswer: '8'
+  }
+];
+
+module.exports = questionArray;
